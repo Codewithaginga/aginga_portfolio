6b8a5af285a9b42222588edbc27cf742cd1cb873
aginga <brianaginga35@gmail.com>
2023-04-05 08:59:34 -0700

    paddings

--- js/aginga.js
+++ js/aginga.js
@@ -12,3 +12,18 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 100);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx");
+    navList.classList.toggle("open");
+
+};
+window.onscroll = () => {
+    menu.classList.remove("bx");
+    navList.classList.remove("open"); 
+};
+
+
