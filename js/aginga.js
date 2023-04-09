e66c3baf6f86306b27ed20f6c68c582647cf2d59
aginga <brianaginga35@gmail.com>
2023-04-09 16:08:20 -0700

    new changes

--- js/aginga.js
+++ js/aginga.js
@@ -1,29 +1,4 @@
-
-const btn = document.querySelector('.btn');
-
-function alertBtn() {
-    alert('This is my peronal Portfolio');
-}
-
-btn.addEventListener('click', alertBtn);
-
 const header = document.querySelector("header");
-
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky", window.scrollY > 100);
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 100);
 });
-
-let menu = document.querySelector("#menu-icon");
-let navList = document.querySelector(".navlist");
-
-menu.onclick = () => {
-    menu.classList.toggle("bx");
-    navList.classList.toggle("open");
-
-};
-window.onscroll = () => {
-    menu.classList.remove("bx");
-    navList.classList.remove("open"); 
-};
-
-
